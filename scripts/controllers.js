1b9344042cfdb15519c322a8670cda2156a78a6d
이용현(LEE YONG HYUN)/물류/서비스사업2팀/SK <05578@skcc.com>
2016-05-19 09:55:48 +0900

    init

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -87,4 +87,112 @@ confusionApp
                     console.log($scope.feedback);
                 }
             };
-        }]);
+        }])
+
+
+        .controller('HomeController',
+            ['$scope',
+            function ($scope) {
+
+            }])
+
+
+        .controller('LoginController',
+            ['$scope', '$rootScope', '$location', 'AuthenticationService',
+            function ($scope, $rootScope, $location, AuthenticationService) {
+                // reset login status
+                AuthenticationService.ClearCredentials();
+                $rootScope.loginName = false;
+
+                $scope.login = function () {
+                    $scope.dataLoading = true;
+                    AuthenticationService.Login($scope.username, $scope.password, function(res) {
+
+                        //alert(response.id);
+                        //alert(response.response.transaction[0].id);
+                        //alert(response.response.dataSet[0].recordSet[0].record[0].ENO);
+
+                        var response = { success: true };
+                        var emp_id = ''; //emp_id
+                        var eno = ''; //사번
+                        var busi_unt_ornz_nm = ''; //팀
+                        var name = ''; //성명
+                        var rsof_nm = ''; //직책
+                        var busi_unt_nm = ''; //사업단위
+                        var pos_nm = ''; //직위
+
+                        if(res.response.dataSet[0].recordSet[0].record == undefined || res.response.dataSet[0].recordSet[0].record == null){
+                            response.success = false;
+                            response.message = 'Username or password is incorrect';
+                        }else{
+                            emp_id = res.response.dataSet[0].recordSet[0].record[0].EMP_ID; //emp_id
+                            eno = res.response.dataSet[0].recordSet[0].record[0].ENO; //사번
+                            busi_unt_ornz_nm = res.response.dataSet[0].recordSet[0].record[0].BUSI_UNT_ORNZ_NM; //팀
+                            name = res.response.dataSet[0].recordSet[0].record[0].NAME; //성명
+                            rsof_nm = res.response.dataSet[0].recordSet[0].record[0].RSOF_NM; //직책
+                            busi_unt_nm = res.response.dataSet[0].recordSet[0].record[0].BUSI_UNT_NM; //사업단위
+                            pos_nm = res.response.dataSet[0].recordSet[0].record[0].POS_NM; //직위
+
+                            $rootScope.name = name;
+                            $rootScope.emp_id = emp_id;
+                            $rootScope.loginName = true;
+
+                        }
+
+                        if(response.success) {
+                            AuthenticationService.SetCredentials($scope.username, $scope.password);
+                            $location.path('/');
+                        } else {
+                            $scope.error = response.message;
+                            $scope.dataLoading = false;
+                        }
+                    });
+                };
+            }])
+
+        .controller('AboutusController',
+            ['$scope','$location',
+            function ($scope) {
+                    $scope.isActive = function (setTabpanel) {
+                        return ($scope.tabpanel === setTabpanel);
+
+                    };
+                    $scope.select = function(setTabpanel) {
+                        $scope.tabpanel = setTabpanel;
+                    };
+            }])
+
+        .controller('OmsController', ['$scope', 'omsFactory', function($scope, omsFactory) {
+
+            $scope.omses = {};
+            omsFactory.getOms()
+            .then(
+                function(response){
+                    $scope.omses = response.data;
+                }
+            );
+
+            $scope.getImage = function () {
+                    omsFactory.getImage($scope.empId, function(res) {
+                    $scope.image = "data:image/gif;base64,"+res.response.dataSet[0].fields[0].EMP_IMGE;
+
+                });
+            };
+
+
+
+                                }])
+
+        .controller('SystemController', ['$scope', 'systemFactory', function($scope, systemFactory) {
+
+            $scope.systems = {};
+            systemFactory.getSystem()
+            .then(
+                function(response){
+                    $scope.systems = response.data;
+                }
+            );
+
+
+
+                                }]);
--- scripts/services.js
+++ scripts/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 confusionApp
-        .constant("baseURL","http://localhost:3000/")
+        .constant("baseURL","http://127.0.0.1:3000/")
         .service('menuFactory', ['$http', 'baseURL', function($http,baseURL) {
 
             this.getDishes = function(){
@@ -12,3 +12,187 @@ confusionApp
             };
 
         }])
+
+
+        .service('omsFactory', ['$http', 'baseURL', function($http,baseURL) {
+
+            this.getOms = function(){
+                return $http.get(baseURL+"oms");
+            };
+
+            this.getImage  = function (empId, callback) {
+            $http.post(baseURL+'api/userImage', { empId: empId})
+                .success(function (data,status,headers, config) {
+
+                    var responseData = JSON.parse(data);
+
+                    callback(responseData);
+                });
+            };
+
+        }])
+
+        .service('systemFactory', ['$http', 'baseURL', function($http,baseURL) {
+
+            this.getSystem = function(){
+                return $http.get(baseURL+"system");
+            };
+
+        }])
+
+
+.factory('AuthenticationService',
+    ['Base64', '$http', '$cookieStore', '$rootScope', '$timeout', 'baseURL',
+    function (Base64, $http, $cookieStore, $rootScope, $timeout, baseURL) {
+        var service = {};
+
+        service.Login = function (username, password, callback) {
+
+            /* Dummy authentication for testing, uses $timeout to simulate api call
+             ----------------------------------------------*/
+
+            /*
+            $timeout(function(){
+                var response = { success: username === 'test' && password === 'test' };
+                if(!response.success) {
+                    response.message = 'Username or password is incorrect';
+                }
+                callback(response);
+            }, 1000);
+            */
+
+            /* Use this for real authentication
+             ----------------------------------------------*/
+            $http.post(baseURL+'api/authenticate', { username: username, password: password })
+                .success(function (data,status,headers, config) {
+
+                    var responseData = JSON.parse(data);
+                    //alert(responseData.response.transaction[0].id);
+                    //var response = { success: true };
+
+                    //emp_id = responseData.response.dataSet[0].recordSet[0].record[0].EMP_ID;
+
+                /*
+                    if(!responseData.response.dataSet.recordSet.record[0].EMP_ID) {
+                        response.message = 'Username or password is incorrect';
+                        response.success = false;
+                    }
+                */
+
+                    callback(responseData);
+                });
+
+
+        };
+
+        service.SetCredentials = function (username, password) {
+            var authdata = Base64.encode(username + ':' + password);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        };
+
+        service.ClearCredentials = function () {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic ';
+        };
+
+        return service;
+    }])
+
+
+
+
+.factory('Base64', function () {
+    /* jshint ignore:start */
+
+    var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    keyStr.charAt(enc1) +
+                    keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) +
+                    keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                window.alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
+
+    /* jshint ignore:end */
+});
